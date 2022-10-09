9e4fc000d60625e527cd789060913ad1721bcd16
kosakae256 <kosaka256@gmail.com>
2022-10-09 09:33:59 +0900

    .

--- app/lib/achive/achive_loader.py
+++ app/lib/achive/achive_loader.py
@@ -18,47 +18,287 @@ def achive_loader():
             ]
         },
         "002": {
-            "title": "魚を初めて釣り上げる2",
-            "max_progress": 2,
-            "rewords_text": ["200xp", "200pt"],
+            "title": "魚を合計10匹釣り上げる",
+            "max_progress": 10,
+            "rewords_text": ["100xp", "100pt"],
             "rewords": [
                 {
                     "id": "xp",
-                    "num": 200,
+                    "num": 100,
                 },
                 {
                     "id": "pt",
-                    "num": 200,
+                    "num": 100,
                 }
             ]
         },
         "003": {
-            "title": "魚を初めて釣り上げる3",
-            "max_progress": 3,
-            "rewords_text": ["300xp", "300pt"],
+            "title": "魚を合計50匹釣り上げる",
+            "max_progress": 50,
+            "rewords_text": ["100xp", "100pt"],
             "rewords": [
                 {
                     "id": "xp",
-                    "num": 300,
+                    "num": 100,
                 },
                 {
                     "id": "pt",
-                    "num": 300,
+                    "num": 100,
                 }
             ]
         },
         "004": {
-            "title": "魚を初めて釣り上げる4",
-            "max_progress": 4,
-            "rewords_text": ["400xp", "400pt"],
+            "title": "魚を合計100匹釣り上げる",
+            "max_progress": 100,
+            "rewords_text": ["100xp", "100pt"],
             "rewords": [
                 {
                     "id": "xp",
-                    "num": 400,
+                    "num": 100,
                 },
                 {
                     "id": "pt",
-                    "num": 400,
+                    "num": 100,
+                }
+            ]
+        },
+        "005": {
+            "title": "サンマを合計30匹釣り上げる",
+            "max_progress": 30,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "006": {
+            "title": "メバルを合計15匹釣り上げる",
+            "max_progress": 15,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "007": {
+            "title": "イワナを合計40匹釣り上げる",
+            "max_progress": 40,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "008": {
+            "title": "タチウオを合計30匹釣り上げる",
+            "max_progress": 30,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "009": {
+            "title": "マゴチを合計10匹釣り上げる",
+            "max_progress": 1,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "010": {
+            "title": "タイを合計20匹釣り上げる",
+            "max_progress": 20,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "011": {
+            "title": "カマスを1匹釣り上げる",
+            "max_progress": 1,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "012": {
+            "title": "スズキを50匹釣り上げる",
+            "max_progress": 50,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "013": {
+            "title": "タナゴを合計30匹釣り上げる",
+            "max_progress": 30,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "014": {
+            "title": "ゴンズイを合計30匹釣り上げる",
+            "max_progress": 30,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "015": {
+            "title": "マグロを20匹釣り上げる",
+            "max_progress": 20,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "016": {
+            "title": "イワシを合計20匹釣り上げる",
+            "max_progress": 20,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "017": {
+            "title": "カサゴを合計40匹釣り上げる",
+            "max_progress": 40,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "018": {
+            "title": "メジナを合計30匹釣り上げる",
+            "max_progress": 30,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "019": {
+            "title": "イシダイを合計10匹釣り上げる",
+            "max_progress": 10,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
+                }
+            ]
+        },
+        "020": {
+            "title": "アジを合計20匹釣り上げる",
+            "max_progress": 20,
+            "rewords_text": ["100xp", "100pt"],
+            "rewords": [
+                {
+                    "id": "xp",
+                    "num": 100,
+                },
+                {
+                    "id": "pt",
+                    "num": 100,
                 }
             ]
         },
--- app/lib/quest/quest_loader.py
+++ app/lib/quest/quest_loader.py
@@ -3,22 +3,256 @@
 def quest_loader():
     quests = {
         "001": {
-            "title": "クエスト1",
-            "content": "クエスト1の目的",
-            "max_progress": 2,
+            "title": "初めての釣り",
+            "content": "魚を一匹釣りあげる",
+            "max_progress": 1,
             "items": [{
                 "id": "900",
-                "amount": 200
+                "amount": 500
             },
             {
                 "id": "901",
-                "amount": 200
+                "amount": 500
             }]
         },
         "002": {
-            "title": "クエスト2",
-            "content": "クエスト2の目的",
-            "max_progress": 4,
+            "title": "海底を駆けるもの",
+            "content": "ハゼを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "003": {
+            "title": "誰にも奴は止められない",
+            "content": "マグロを一匹釣り上げる",
+            "max_progress": 1,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "004": {
+            "title": "海の厄介者たち",
+            "content": "クサフグまたはゴンズイを合計5匹釣り上げる",
+            "max_progress": 5,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "005": {
+            "title": "可愛いものにも毒がある",
+            "content": "ゴンズイを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "006": {
+            "title": "秋の名刀を手に入れろ",
+            "content": "サンマを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "007": {
+            "title": "美味なる黒",
+            "content": "ブラックバスを1匹釣り上げる",
+            "max_progress": 1,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "008": {
+            "title": "陰と陽",
+            "content": "タイとクロダイをそれぞれ1匹釣り上げる",
+            "max_progress": 2,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "009": {
+            "title": "海の女王",
+            "content": "キスを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "010": {
+            "title": "餌取り名人",
+            "content": "カワハギを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "011": {
+            "title": "ざわめく海",
+            "content": "イワシを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "012": {
+            "title": "穴釣りといえば...",
+            "content": "カサゴを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "013": {
+            "title": "春を告げる魚",
+            "content": "メバルを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "014": {
+            "title": "熱気に勝るもの",
+            "content": "ウナギを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "015": {
+            "title": "渓流の王",
+            "content": "イワナを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "016": {
+            "title": "渓流の女王",
+            "content": "ヤマメを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "017": {
+            "title": "陰の偽物",
+            "content": "メジナを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "018": {
+            "title": "鮒に始まり鮒に終わる",
+            "content": "フナを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "019": {
+            "title": "音を立てるな",
+            "content": "オイカワを3匹釣り上げる",
+            "max_progress": 3,
+            "items": [{
+                "id": "900",
+                "amount": 300
+            },
+            {
+                "id": "901",
+                "amount": 300
+            }]
+        },
+        "020": {
+            "title": "清流の女王",
+            "content": "アユを1匹釣り上げる",
+            "max_progress": 3,
             "items": [{
                 "id": "900",
                 "amount": 300
